045473e6bee3e563db550c3767ba71262f4db89c
Lawrence Francell <lawrence@twintails.com>
2017-04-11 07:47:25 -0500

    search componenet tests

---
+++ app/tests/components/ToDoSearch.test.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactTestUtils from 'react-addons-test-utils'
+
+import expect from 'expect'
+
+import ToDoSearch from 'ToDoSearch'
+
+
+describe('ToDoSearch', () => {
+  it('Should exist', () => {
+    expect(ToDoSearch).toExist()
+  })
+
+  it('Should call onSearch with enterend input text', () => {
+    let searchQuery = 'Dog'
+    let spy = expect.createSpy()
+    let todoSearch = ReactTestUtils.renderIntoDocument(<ToDoSearch onSearch={spy}/>)
+
+    todoSearch.refs.search.value = searchQuery
+    ReactTestUtils.Simulate.change(todoSearch.refs.search)
+
+    expect(spy).toHaveBeenCalledWith(false, 'Dog')
+  })
+
+  it('Should call onSearch with proper checked value', () => {
+    let spy = expect.createSpy()
+    let todoSearch = ReactTestUtils.renderIntoDocument(<ToDoSearch onSearch={spy}/>)
+
+    todoSearch.refs.showCompleted.checked = true
+    ReactTestUtils.Simulate.change(todoSearch.refs.showCompleted)
+
+    expect(spy).toHaveBeenCalledWith(true, '')
+  })
+})
