41c092ffc052b8bef04cf56d1b513df6f35a4b10
Lawrence Francell <lawrence@twintails.com>
2017-05-25 16:04:06 -0500

    update web pack for speedier development

--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,25 @@ module.exports = {
   externals: {
     jquery: 'jQuery'
   },
-  plugins: [
+  plugins: process.env.NODE_ENV === "development" ? [
+    new webpack.ProvidePlugin({
+      '$':'jquery',
+      'jQuery':'jquery'
+    }),
+    extractHTML,
+    extractSCSS,
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV:            JSON.stringify(process.env.NODE_ENV),
+        API_KEY:             JSON.stringify(process.env.API_KEY),
+        AUTH_DOMAIN:         JSON.stringify(process.env.AUTH_DOMAIN),
+        DATABASE_URL:        JSON.stringify(process.env.DATABASE_URL),
+        PROJECT_ID:          JSON.stringify(process.env.PROJECT_ID),
+        STORAGE_BUCKET:      JSON.stringify(process.env.STORAGE_BUCKET),
+        MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID)
+      }
+    })
+  ] : [
     new webpack.ProvidePlugin({
       '$':'jquery',
       'jQuery':'jquery'
